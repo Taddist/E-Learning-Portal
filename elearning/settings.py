269045138504947550c5c2f2c44e2903d600cdcc
Taddist <afaf.taddist71@gmail.com>
2017-12-11 16:01:26 +0000

    add changes

--- elearning/settings.py
+++ elearning/settings.py
@@ -56,6 +56,7 @@ AUTH_USER_MODEL='students.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'elearning','static'),]
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
 LOGIN_REDIRECT_URL='/'
 
 REST_FRAMEWORK={
